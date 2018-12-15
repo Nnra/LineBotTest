a0d2f346c3891cda3cb0e00c984c8b3822fcf7cd
Nnra <nura721@gmail.com>
2018-12-15 15:51:58 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var linebot = require('linebot');
+var express = require('express');
+
+var bot = linebot({
+  channelId: 1631308823,
+  channelSecret: e49c17e8512f087ef0e86af200422e10,
+  channelAccessToken: dg0tRYFjXWaxI7h6jYw/kIY80i1G2WnafieQM6Wn3QLSQmAHcsiE++Wf+V6ZH/cW0bvPFcS0zX4x+BSB0OD0h5I/SqL0NpXBmLomnJNHY6BkAHQmr6/LGmVyoZEY9ZclVJT92ekp7Lb5TgOksCrrgwdB04t89/1O/w1cDnyilFU=
+});
+
+
+bot.on('message', function(event) {
+    console.log(event); //把收到訊息的 event 印出來看看
+  });
+  
+  const app = express();
+  const linebotParser = bot.parser();
+  app.post('/', linebotParser);
+  
+  //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+  var server = app.listen(process.env.PORT || 1337, function() {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
